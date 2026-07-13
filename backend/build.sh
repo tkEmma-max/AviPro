25ec2e9b9eb04b0b42a0828f98ffb5292ea6a749
tamedjoh emmanuel <tkemma585@gmail.com>
2026-07-13 10:54:14 +0100

    corection du build

--- backend/build.sh
+++ backend/build.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-# build.sh
 
-echo "🚀 Installation des dépendances..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "📦 Collecte des fichiers statiques..."
 python manage.py collectstatic --noinput
-
-echo "🗄️  Suppression des anciennes migrations..."
-rm -f */migrations/0*.py
-
-echo "🗄️  Recréation des migrations..."
-python manage.py makemigrations
-
-echo "🗄️  Application des migrations..."
 python manage.py migrate --noinput
-
-echo "✅ Build terminé avec succès !"
