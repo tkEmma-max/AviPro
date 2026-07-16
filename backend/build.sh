6df6bacd554984888fce90c34b1d5cc4f1a44a17
tamedjoh emmanuel <tkemma585@gmail.com>
2026-07-16 12:46:02 +0100

    espoir 3

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,9 @@ echo "📦 Installation des dépendances..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "🔄 Migrations..."
+python manage.py migrate --noinput
+
 echo "📁 Collecte des fichiers statiques..."
 python manage.py collectstatic --noinput
 
